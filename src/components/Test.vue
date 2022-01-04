9920d0ecc90fc0038172ae70f8d4b7bd7dac95bf
Ray <ray@fillygaming.com>
2022-01-04 14:02:20 +0800

    add input

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,16 +1,42 @@
 <template>
-<p>Just a component Test</p>
+<div class="inputBox">
+  <label for="enterSomething">Enter something</label>
+  <input type="text" name="" id="enterSomething" class="input" v-model="inputText">
+</div>
+
+<p>{{ inputText }}</p>
 </template>
 
 <script>
-import {defineComponent} from "vue";
+import {defineComponent, reactive, toRefs} from "vue";
 
 export default defineComponent({
   name: "Test",
+  setup(){
+    const state = reactive({
+      inputText: ""
+    })
+    return{
+      ...toRefs(state)
+    }
+
+  },
   
 })
 </script>
 
 <style scoped>
+.inputBox{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
+.input{
+  width: 240px;
+  padding: 16px;
+  font-size: 24px;
+  border: 1px solid rgb(129, 127, 127);
+  border-radius: 10px;
+}
 </style>
