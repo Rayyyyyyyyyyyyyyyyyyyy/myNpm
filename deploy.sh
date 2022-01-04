45d9c7c01744b06ff27e4e9857032a3182276a0d
Ray <ray@fillygaming.com>
2022-01-04 10:07:02 +0800

    1.0.1

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 构建
-npm run build
+yarn build:doc
 
 # cd 到构建输出的目录下
 cd dist
@@ -13,13 +13,8 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
-git add -A
+git add .
 git commit -m 'deploy'
-
-# 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://github.com/rayyyyyyyyyyyyyyyyyyyy/myNpm.git master:gh-pages
 
 cd -
